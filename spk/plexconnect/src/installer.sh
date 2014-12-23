a936b7aee83b3b3a735b15325cf8f66759bb3d40
Bert Wynants <bwynants+drupal-git@gmail.com>
2014-12-23 21:31:47 +0100

    fixed vhost file

--- spk/plexconnect/src/installer.sh
+++ spk/plexconnect/src/installer.sh
@@ -72,13 +72,18 @@ postinst ()
   sed -i -e "s|%certfile%|${INSTALL_DIR}/etc/certificates/${cert_name}.pem|g" "${CFG_FILE}"
 
 cat <<EOF >"${INSTALL_DIR}/etc/${PACKAGE}-vhost.conf"
+NameVirtualHost *:80
+<VirtualHost *:80>
     ServerName    ${pc_host_name}
     ServerAlias   atv.plexconnect
     ProxyPreserveHost On
     ProxyPass   / http://${sIPNAS}:81/ nocanon
     ProxyPassReverse  / http://${sIPNAS}:81/
+</VirtualHost>
 EOF
 cat <<EOF >"${INSTALL_DIR}/etc/${PACKAGE}-ssl-vhost.conf"
+NameVirtualHost *:443
+<VirtualHost *:443>
     ServerName          ${pc_host_name}
     ServerAlias         atv.plexconnect
     SSLEngine           On
@@ -89,6 +94,7 @@ cat <<EOF >"${INSTALL_DIR}/etc/${PACKAGE}-ssl-vhost.conf"
     ProxyPreserveHost   On
     ProxyPass           / https://${sIPNAS}:444/
     ProxyPassReverse    / https://${sIPNAS}:444/
+</VirtualHost>
 EOF
 
   # create symbolic links
